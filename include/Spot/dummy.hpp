319bba45677c7ddf0452a2cffe136525e90052e4
Kartik Kumar <me@kartikkumar.com>
2015-04-20 16:56:11 +0100

    Initialize Spot library with code based on Cpp-Project (https://github.com/kartikkumar/cpp-project).

---
+++ include/Spot/dummy.hpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2014-2015 Kartik Kumar, Dinamica Srl
+ * Copyright (c) 2014-2015 Marko Jankovic, DFKI GmbH
+ * Copyright (c) 2014-2015 Natalia Ortiz, University of Southampton
+ * Copyright (c) 2014-2015 Juan Romero, University of Strathclyde
+ * Distributed under the MIT License.
+ * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+ */
+
+#ifndef SPOT_H
+#define SPOT_H
+
+#include "Spot/dummy.hpp"
+
+namespace spot
+{
+
+//! Execute dummy function.
+/*!
+ * Executes dummy function. This function can be removed as soon as there is content in the
+ * library. It's used to set up a dummy unit test.
+ *
+ * @return True
+ */
+bool executeDummy( );
+
+} // namespace spot
+
+#endif // SPOT_H
